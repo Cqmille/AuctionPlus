eda798d9b6e1e00f60231df8f80bcb405b004a49
Cqmille <70196927+Cqmille@users.noreply.github.com>
2022-12-10 17:37:26 +0100

    bll / dal update

--- src/main/java/fr/auctionplus/app/bll/AuctionManager.java
+++ src/main/java/fr/auctionplus/app/bll/AuctionManager.java
@@ -1,4 +1,66 @@
 package fr.auctionplus.app.bll;
 
+import fr.auctionplus.app.dal.AuctionDAO;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+
+import fr.auctionplus.app.bo.Auction;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Properties;
+
 public class AuctionManager {
+    // The single instance of the AuctionManager class
+    private static volatile AuctionManager instance = null;
+
+    // The AuctionDAO instance that is used to persist Auction objects to the database
+    private AuctionDAO auctionDAO;
+
+    // Private constructor to prevent direct instantiation of the AuctionManager class
+    private AuctionManager(AuctionDAO auctionDAO) {
+        this.auctionDAO = auctionDAO;
+    }
+
+    public static synchronized AuctionManager getInstance() throws IOException {
+        if (instance == null) {
+            // Create an EntityManagerFactory using the values from the app.properties file
+            EntityManagerFactory emf = createEntityManagerFactory();
+
+            // Create a new AuctionManager instance with the specified AuctionDAO instance
+            // and store it in the instance field
+            instance = new AuctionManager(AuctionDAO.getInstance(emf));
+        }
+
+        // Return the AuctionManager instance
+        return instance;
+    }
+
+    // Creates an EntityManagerFactory using the values from the app.properties file
+    private static EntityManagerFactory createEntityManagerFactory() throws IOException {
+        // Read the values from the app.properties file
+        Properties appProperties = new Properties();
+        ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
+        appProperties.load(classLoader.getResourceAsStream("app.properties"));
+
+        String databaseUrl = appProperties.getProperty("database.url");
+        String databaseDriver = appProperties.getProperty("database.driver");
+        String databaseUsername = appProperties.getProperty("database.username");
+        String databasePassword = appProperties.getProperty("database.password");
+
+        // Use the values from the app.properties file to create an EntityManagerFactory
+        Map<String, String> properties = new HashMap<>();
+        properties.put("jakarta.persistence.jdbc.url", databaseUrl);
+        properties.put("jakarta.persistence.jdbc.driver", databaseDriver);
+        properties.put("jakarta.persistence.jdbc.user", databaseUsername);
+        properties.put("jakarta.persistence.jdbc.password", databasePassword);
+
+        return Persistence.createEntityManagerFactory("default", properties);
+    }
+
+    public void save(Auction auction) {
+        auctionDAO.save(auction);
+    }
+
 }
--- src/main/java/fr/auctionplus/app/dal/AuctionDAO.java
+++ src/main/java/fr/auctionplus/app/dal/AuctionDAO.java
@@ -1,4 +1,54 @@
 package fr.auctionplus.app.dal;
 
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.PersistenceUnit;
+
+import fr.auctionplus.app.bo.Auction;
+
 public class AuctionDAO {
+    // The single instance of the AuctionDAO class
+    private static volatile AuctionDAO instance = null;
+
+    // The EntityManagerFactory instance that is used to create EntityManager objects
+    @PersistenceUnit(name = "default")
+    private EntityManagerFactory emf;
+
+    // Private constructor to prevent direct instantiation of the AuctionDAO class
+    private AuctionDAO(EntityManagerFactory emf) {
+        this.emf = emf;
+    }
+
+    // Public synchronized static method that returns the single AuctionDAO instance
+    public static AuctionDAO getInstance(EntityManagerFactory emf) {
+        if (instance == null) {
+            // Create a new AuctionDAO instance with the specified EntityManagerFactory
+            // and store it in the instance field
+            instance = new AuctionDAO(emf);
+        }
+
+        // Return the AuctionDAO instance
+        return instance;
+    }
+
+    public void save(Auction auction) {
+        // Create an EntityManager object using the EntityManagerFactory
+        EntityManager em = emf.createEntityManager();
+
+        // Begin a transaction
+        em.getTransaction().begin();
+
+        // Persist the Auction object to the database
+        em.persist(auction);
+
+        // Commit the transaction
+        em.getTransaction().commit();
+
+        // Print a message to the console indicating that the auction was saved to the database
+        System.out.println("Ajouté en base : " + auction);
+
+        // Close the EntityManager
+        em.close();
+    }
+
 }
